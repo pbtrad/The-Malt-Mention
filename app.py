f75e11cd91a9907c410055f53ab5ba0cb7913f73
pbtrad <paulbrowne2@gmail.com>
2020-12-08 09:07:03 +0000

    Added registration form and set up werkzeug password_hash.

--- app.py
+++ app.py
@@ -2,27 +2,37 @@ import os
 from flask import (
     Flask, url_for, render_template,
      redirect, request, session)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
+
+
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template("home.html")
+    posts = mongo.db.posts.find()
+    return render_template("home.html", posts=posts)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
 
 
 @app.route("/login")
 def login():
     return render_template("login.html")
-
-
-
-@app.route("/register")
-def register():
-    return render_template("register.html")
 
 
 
