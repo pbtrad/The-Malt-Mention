c2532e414cdba6e20e9eb7a78d34e75c7034db79
pbtrad <paulbrowne2@gmail.com>
2021-01-08 21:26:12 +0000

    added wireframes and flowcharts

--- app.py
+++ app.py
@@ -188,4 +188,4 @@ def edit_profile(user_id):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
